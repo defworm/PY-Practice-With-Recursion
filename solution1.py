9b4df8976c79bd6892a9756a6668b5fb58918282
Crystal Korth <101788833+defworm@users.noreply.github.com>
2022-11-02 20:20:41 -0500

    completed 1-3

--- solution1.py
+++ solution1.py
@@ -1,1 +1,13 @@
-# Write code for algorithm 1 below
+# Write code for algorithm 1 below: Write a program that takes a positive number as an argument and prints the numbers from n to zero.
+
+# We want to define a function that takes a number (n), prints it, and calls itself again with the value of n-1.
+
+def count_down(n):
+    if n == 0:
+        return 
+    else:
+        print(n)
+        count_down(n-1)
+
+n=4
+count_down(n)
--- solution3.py
+++ solution3.py
@@ -1,1 +1,20 @@
-# Write code for algorithm 3 below
+# Write code for algorithm 3 below: write a function that returns the nth elements in the Fibonacci Sequence
+
+# 0, 1,1,2,3,4,8,13,21
+
+def nth_fibbonacci(n):
+    if n<= 0:
+        print("incorrect input")
+    elif n ==1:
+        return 0
+    elif n ==2:
+        return 1
+    else:
+        return nth_fibbonacci(n-1)+nth_fibbonacci(n-2)
+print("2nd fib number:")
+print(nth_fibbonacci(2))
+print("4th fib number:")
+print(nth_fibbonacci(4))
+print("8th fib number:")
+print(nth_fibbonacci(8))
+    
